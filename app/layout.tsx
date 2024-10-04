dc1527cb2c8dd395b7fb189df558c49d1f62e147
divyadharshana5 <divyadharshana3@gmail.com>
2024-10-04 20:50:36 +0530

    Initialize the shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { cn } from "./lib/utils";
 
 const fontSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
-  weight: [`300`, `400`, `500`, `600`, `700`],
-  variable: `--font-sans`,
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
