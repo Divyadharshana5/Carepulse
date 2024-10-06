74b23aaed1a3c5a0f4e0a1f0e244df904f8558b2
divyadharshana5 <divyadharshana3@gmail.com>
2024-10-06 18:22:29 +0530

    import PatientForm

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import PatientForm from "@/components/forms/PatientForm";
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image />
+          <PatientForm />
+          <div className="text-14-regular"></div>
         </div>
       </section>
     </div>
